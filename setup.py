be4f815bd7a50027949b2c2f9437b51b2690eea3
Stephen Mayhew <swm.mayhew@gmail.com>
2017-09-26 15:07:53 -0400

    Fixed typos in setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(name='conll',
       version='0.1',
-      description='The funniest joke in the world',
-      url='http://github.com/mayhew2/python-conll',
-      author='Scripts for NER CoNLL format files',
+      description='A collection of scripts for working with conll format files.',
+      url='http://github.com/mayhewsw/python-conll',
+      author='Stephen Mayhew',
       author_email='swm.mayhew@gmail.com',
       license='MIT',
       packages=['conll'],
